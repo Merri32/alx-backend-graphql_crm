c121ecf9284b6d164b182565e66ebcd75efbb90b
Meron Bihon <meronbihon13@gmail.com>
2025-10-20 09:47:19 +0300

    Add missing schema.py and settings.py

---
+++ alx_backend_graphql/settings.py
@@ -0,0 +1,30 @@
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+SECRET_KEY = 'replace-with-your-secret-key'
+DEBUG = True
+ALLOWED_HOSTS = []
+
+INSTALLED_APPS = [
+    'django.contrib.admin',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
+    'django.contrib.sessions',
+    'django.contrib.messages',
+    'django.contrib.staticfiles',
+    'graphene_django',
+]
+
+GRAPHENE = {
+    "SCHEMA": "alx_backend_graphql.schema.schema"
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+STATIC_URL = '/static/'
